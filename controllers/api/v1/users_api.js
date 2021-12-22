297eaca71f112f4e88d7edbe5a10e619f08e9334
coldfish <pubg69089@gmail.com>
2021-12-22 21:53:42 +0530

    set up passport-jwt directory, installed passport-jwt and jsonwebtoken

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,25 @@
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+module.exports.createSession =async function(req,res){
+    try{
+
+        let user =await User.findOne({email : req.body.email});
+        if(!user || user.password !=req.body.password){
+            return res.json(422,{
+                message : "invalid username or password"
+
+            });
+        }
+        return res.json(200,{
+            message : "sign in successfull , here is your tocken .. please keep it safe",
+            data : {
+                token : jwt.sign(user.toJSON(),'codial',{expiresIn : '10000'})
+            }
+        })
+    } catch(err){
+        return res.json(200,{
+            message : "internal server error"
+        });
+    }
+    
+}
