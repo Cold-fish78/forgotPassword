4cb8ac7e22743ea43f63024ea416981021276d6d
coldfish <pubg69089@gmail.com>
2021-12-24 21:39:18 +0530

    user can now signin/signup using his google account

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const googleStrategy = require('passport-googel-oauth').Oauth2Strategyc;
+const googleStrategy = require('passport-google-oauth20').Strategy;
 const crypto = require('crypto');
 const User = require('../models/user');
 
@@ -11,30 +11,30 @@ passport.use(new googleStrategy({
 }, function (accessToken, refreshToken, profile, done) {
     // find a user
     User.findOne({
-        email: profile.emails[0].value.exec(function (err, user) {
-            if (err) {
-                console.log("error at googel Strategy" + err);
-                return;
-            }
-            console.log(profile);
-            // if found , set this user as req.user
-            if (user) {
+        email: profile.emails[0].value
+    }).exec(function (err, user) {
+        if (err) {
+            console.log("error at googel Strategy" + err);
+            return;
+        }
+        console.log(profile);
+        // if found , set this user as req.user
+        if (user) {
+            return done(null, user);
+        } else { 
+            // if not found, create the user and set is as req.user
+            User.create({
+                name: profile.displayName,
+                email: profile.emails[0].value,
+                password: crypto.randomBytes(20).toString('hex')
+            }, function (err, user) {
+                if (err) {
+                    console.log("error in google strategy line 27" + err);
+                    return;
+                }
                 return done(null, user);
-            } else { 
-                // if not found, create the user and set is as req.user
-                User.create({
-                    name: profile.displayName,
-                    email: profile.emails[0].value,
-                    password: crypto.randomBytes(20).toString('hex')
-                }, function (err, user) {
-                    if (err) {
-                        console.log("error in google strategy line 27" + err);
-                        return;
-                    }
-                    return done(null, user);
-                })
-            }
-        })
+            })
+        }
     })
 }
 ));
